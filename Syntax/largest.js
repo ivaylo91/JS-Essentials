613ffbf8db4fe5d7168b116d60547e9e98c37548
ivaylo91 <ipenev91@gmail.com>
2019-10-18 14:59:32 +0300

    Print Largest Number

--- Syntax/largest.js
+++ Syntax/largest.js
@@ -1,6 +1,10 @@
 function solve(...params) {
 
     return `Largest number is ${params.sort((a,b)=>a-b).pop()}`;
+    /**
+     * Second variant to solve
+     *  return `Largest numbers is ${Math.max(...params)}`
+     */
 }
 
-console.log(solve(5,-3,16));
+console.log(solve(5,-3,16,18,20));

d5d481af737cb25ff4cf077a82abd2a33bbd9344
ivaylo91 <ipenev91@gmail.com>
2019-10-18 17:06:27 +0300

    Print day of week

---
+++ Syntax/dayofWeek.js
@@ -0,0 +1,15 @@
+const daysMap ={
+    "Monday":1,
+    "Tuesday":2,
+    "Wednesday":3,
+    "Thursday":4,
+    "Friday":5,
+    "Saturday":6,
+    "Sunday":7
+};
+function solve(day){
+    return daysMap[day]?daysMap[day]:"error";
+}
+console.log(solve("Monday"),
+    solve("Friday"),
+    solve("Invalid"));

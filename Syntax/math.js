6ae5caaf23b7715317a6b3d25edf3ce3a566ff89
ivaylo91 <ipenev91@gmail.com>
2019-10-18 11:29:16 +0300

    Initial Commit JS Syntax

--- Syntax/math.js
+++ Syntax/math.js
@@ -6,7 +6,9 @@ const operationMap = {
     "%":(x,y)=>x%y
 };
 function solve(operation, ...params){
-     return params.reduce((a,b)=>eval(`${a} ${operation} ${b}`),params.shift());
+     return params.reduce((a,b)=>operationMap[operation](
+         a,Number(b)
+     ),Number(params.shift()));
 }
 
 console.log(solve("+",5,6,4,5,225,30,29));
